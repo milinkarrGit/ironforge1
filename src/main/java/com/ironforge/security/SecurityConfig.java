77f319ffcad3a0d6c4831ea277637f9c7188f3d0
milinkarrGit <janviermilinkarr@gmail.com>
2026-04-29 09:34:52 -0400

    Fix programmes permitAll

--- src/main/java/com/ironforge/security/SecurityConfig.java
+++ src/main/java/com/ironforge/security/SecurityConfig.java
@@ -104,6 +104,7 @@ public class SecurityConfig {
         return http.build();
     }
 
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
